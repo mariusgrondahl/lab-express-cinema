fdef8eeccb13cbc7ba27ec54df2ecb5dbf9e6eb2
Marius Grondahl <marius@grondahldesign.no>
2019-07-19 09:37:57 +0200

    First commot

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,35 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/Movie.js');
+
+
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+/* GET Movies */
+router.get('/movies', (req, res, next) => {
+  Movie.find({})
+    .then(Movies => {
+      res.render('movies', {Movies});
+    })
+    .catch(error => {
+      console.log(error)
+    })
+});
+
+/* Single Movie */
+router.get('/movies/:movieId', (req, res, next) => {
+  Movie.findById(req.params.movieId)
+    .then(Movies => {
+      res.render('single-movie', {Movies});
+
+    })
+    .catch(error => {
+      console.log(error)
+    })
 });
 
 module.exports = router;
